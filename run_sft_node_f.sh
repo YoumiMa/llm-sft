b33823f695401986ca45191524339d5ab9ef7a2c
YoumiMa <youmima1015@gmail.com>
2025-06-27 17:24:18 +0900

    updated runnig script

--- run_sft_node_f.sh
+++ run_sft_node_f.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
 #$ -cwd
 #$ -l node_f=1
-#$ -l h_rt=24:00:00
+#$ -l h_rt=20:00:00
 
 HUGGINGFACE_CACHE=/gs/bs/tga-okazaki/ma/cache
 
 export HUGGINGFACE_HUB_CACHE=$HUGGINGFACE_CACHE
 export HF_HOME=$HUGGINGFACE_CACHE
+
+#export NCCL_IB_DISABLE=1
+#export NCCL_SOCKET_IFNAME=br01 # for me it is 'br0' interface, you should use yours :)
+export NCCL_P2P_DISABLE=1
+export NCCL_DEBUG=INFO
 
 eval "$(/apps/t4/rhel9/free/miniconda/24.1.2/bin/conda shell.bash hook)"
 conda activate llm-jp-sft
@@ -14,18 +19,20 @@ conda activate llm-jp-sft
 LR=2.5e-5
 MINLR=2.5e-6
 WD=0.1
-NAME=llama-3.1-swallow_lmsys_adamw_0.95_LR_${LR}_MINLR_${MINLR}_WD_${WD}_EPOCH_2
+NAME=$1; shift
+SEED=$1; shift
+DATA=("$@")
+NAME=llama-3.1-${NAME}_LR_${LR}_MINLR_${MINLR}_WD_${WD}
 
-
-accelerate launch --config_file configs/my_accelerate_config_zero3.yaml train_llm.py --output_dir /gs/bs/tga-okazaki/ma/$NAME \
+accelerate launch --config_file configs/my_accelerate_config_zero1.yaml scripts/train_llm_llama3.py --output_dir /gs/bs/tga-okazaki/ma/ckpts/${NAME}_${SEED} \
 --run_name $NAME \
---data_files /gs/bs/tga-okazaki/ma/lmsys-chat-1m/lmsys-chat-1m-synth-sft.jsonl.gz  \
---model_name_or_path tokyotech-llm/Llama-3.1-Swallow-8B-v0.1 \
---tokenizer_name_or_path tokyotech-llm/Llama-3.1-Swallow-8B-Instruct-v0.1 \
+--data_files ${DATA[*]} \
+--model_name_or_path meta-llama/Llama-3.1-8B \
+--tokenizer_name_or_path meta-llama/Llama-3.1-8B-Instruct \
 --bf16 \
 --num_train_epochs 2 \
---per_device_train_batch 4 \
---gradient_accumulation_steps 16 \
+--per_device_train_batch 2 \
+--gradient_accumulation_steps 64 \
 --gradient_checkpointing \
 --optim adamw_torch \
 --adam_beta2 0.95 \
@@ -35,5 +42,5 @@ accelerate launch --config_file configs/my_accelerate_config_zero3.yaml train_ll
 --weight_decay ${WD} \
 --warmup_ratio 0.1 \
 --logging_steps 10 \
---save_steps 300 \
-
+--save_steps 500 \
+--seed ${SEED} \
